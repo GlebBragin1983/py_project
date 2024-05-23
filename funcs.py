1780769a7a96c779d543099a40da92b54d6e19a5
GlebBragin1983 <natajuve83@gmail.com>
2024-05-23 20:44:45 +0300

    Add second fun

--- funcs.py
+++ funcs.py
@@ -3,3 +3,11 @@ def sum_fun(num_1, num_2):
     Возвращает сумму чисел.
     """
     return num_1 + num_2
+
+
+def diff_fun(num_1, num_2):
+    """
+    Возвращает разность чисел.
+    """
+    return num_1 - num_2
+
